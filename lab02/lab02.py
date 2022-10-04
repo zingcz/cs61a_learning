91be1847e3e0b7f65a9cc47c1d340401809628f1
zingcz <78351817+zingcz@users.noreply.github.com>
2022-10-04 17:52:21 +0800

    first summary
    basic code i write for the lab

---
+++ lab02/lab02.py
@@ -0,0 +1,153 @@
+from threading import local
+
+
+def add(x,y):
+    return x + y
+def lambda_curry2(func):
+    """
+    Returns a Curried version of a two-argument function FUNC.
+    >>> from operator import add, mul, mod
+    >>> curried_add = lambda_curry2(add)
+    >>> add_three = curried_add(3)
+    >>> add_three(5)
+    8
+    >>> curried_mul = lambda_curry2(mul)
+    >>> mul_5 = curried_mul(5)
+    >>> mul_5(42)
+    210
+    >>> lambda_curry2(mod)(123)(10)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    #return lambda f,x :lambda y:f(x,y)
+    return (lambda func:lambda x:lambda y:func(x,y))(func)
+    ##in the lambda_curry2 function we get formal parameter past in and then it return a funcion ,
+    ##we use the return function to get the second value,so the lambda
+    ##function we write should take the parameter past in and turn the second lambda function to accept the second value,perfect!
+
+
+
+def count_cond(condition):
+    """Returns a function with one parameter N that counts all the numbers from
+    1 to N that satisfy the two-argument predicate function Condition, where
+    the first argument for Condition is N and the second argument is the
+    number from 1 to N.
+
+    >>> count_factors = count_cond(lambda n, i: n % i == 0)
+    >>> count_factors(2)   # 1, 2
+    2
+    >>> count_factors(4)   # 1, 2, 4
+    3
+    >>> count_factors(12)  # 1, 2, 3, 4, 6, 12
+    6
+
+    >>> is_prime = lambda n, i: count_factors(i) == 2
+    >>> count_primes = count_cond(is_prime)
+    >>> count_primes(2)    # 2
+    1
+    >>> count_primes(3)    # 2, 3
+    2
+    >>> count_primes(4)    # 2, 3
+    2
+    >>> count_primes(5)    # 2, 3, 5
+    3
+    >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
+    8
+    """
+    "*** YOUR CODE HERE ***"
+    def condition_check(check_number):
+        i ,count = 1 , 0
+        while i <= check_number:
+            if condition(check_number,i):
+                count = count + 1
+            i = i + 1
+        return count
+    return condition_check
+
+
+
+
+
+def compose1(f, g):
+    """Return the composition function which given x, computes f(g(x)).
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> a1 = compose1(square, add_one)   # (x + 1)^2
+    >>> a1(4)
+    25
+    >>> mul_three = lambda x: x * 3      # multiplies 3 to x
+    >>> a2 = compose1(mul_three, a1)    # ((x + 1)^2) * 3
+    >>> a2(4)
+    75
+    >>> a2(5)
+    108
+    """
+    return lambda x: f(g(x))
+
+def composite_identity(f, g):
+    """
+    Return a function with one parameter x that returns True if f(g(x)) is
+    equal to g(f(x)). You can assume the result of g(x) is a valid input for f
+    and vice versa.
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> b1 = composite_identity(square, add_one)
+    >>> b1(0)                            # (0 + 1)^2 == 0^2 + 1
+    True
+    >>> b1(4)                            # (4 + 1)^2 != 4^2 + 1
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    return lambda x:f(g(x)) == g(f(x))
+
+
+
+def cycle(f1, f2, f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    "*** YOUR CODE HERE ***"
+    def take_nubmber_turns(turns):
+        def take_computation(x):
+            local_turns = turns
+            while local_turns > 0:
+                if local_turns > 0:
+                    x = f1(x)
+                    local_turns = local_turns - 1
+                if local_turns > 0:
+                    x = f2(x)
+                    local_turns = local_turns - 1
+                if local_turns > 0:
+                    x = f3(x)
+                    local_turns = local_turns -1
+            return x
+        return take_computation
+    return take_nubmber_turns
+    ##if we want to design a single argument function which take only one formal parameter once
+    ##we must let every function return the closer inline function.so that we can call the function mutiply times to get parameter
+
+
